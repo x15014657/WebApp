a0bf14a57d764a221c1208e607c83c66542ecfac
x15014657 <x15014657@student.ncirl.ie>
2016-12-02 17:18:37 -0500

    conor

--- contact.php
+++ contact.php
@@ -58,13 +58,6 @@
 		</div>
 	</nav>
 
-		<?php
-         	 require_once('recaptchalib.php');
-          $publickey = "6Ldsrw0UAAAAAJ76-56yX16JTjPk9NezNI_blD0a"; // you got this from the signup page
-          echo recaptcha_get_html($publickey);
-      			  ?>
-					<?php
-		
 		
 				if(isset($_POST['email'])){
 					
