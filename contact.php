518e9f0bd3de43f110dd9cb0d889d03c7700f557
x15014657 <x15014657@student.ncirl.ie>
2016-12-02 17:11:33 -0500

    php recaptcha

--- contact.php
+++ contact.php
@@ -1,60 +1,69 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<title>Music-Zone</title>
-		
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta name="description" content="Video/Audio On-line Shop">
-		<meta name="keywords" content="Videos, Albumes, Music, Shop, On-line, Entertainment">
-		<meta name="author" content="Team J">
-		
-		<link rel="stylesheet" href="css/bootstrap.min.css">
-		<link rel="stylesheet" href="css/bootstrap-theme.min.css">
-	
-		<script src="http://code.jquery.com/jquery-1.11.3.min.js"> </script>
-		<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"> </script>
-		<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"> </script>
-		<script src='https://www.google.com/recaptcha/api.js'></script>
-	</head>
-	
-	
-	<body>
-		<div class="container-fluid">
-			<div class="row padding">
-				<div class="col-md-4"></div>
-				<div class="col-md-offset-1"></div>
-				<div class="col-md-2"><img src="img/logo-bird.png" alt="logo-video shop"></div>
-				<div class="col-md-offset-1"></div>
-				<div class="col-md-4"></div>
-			</div>
+
+<head>
+	<title>Music-Zone</title>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Video/Audio On-line Shop">
+	<meta name="keywords" content="Videos, Albumes, Music, Shop, On-line, Entertainment">
+	<meta name="author" content="Team J">
+
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+
+	<script src="http://code.jquery.com/jquery-1.11.3.min.js">
+	</script>
+	<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js">
+	</script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js">
+	</script>
+	<script src='https://www.google.com/recaptcha/api.js'></script>
+</head>
+
+
+<body>
+	<div class="container-fluid">
+		<div class="row padding">
+			<div class="col-md-4"></div>
+			<div class="col-md-offset-1"></div>
+			<div class="col-md-2"><img src="img/logo-bird.png" alt="logo-video shop"></div>
+			<div class="col-md-offset-1"></div>
+			<div class="col-md-4"></div>
 		</div>
-	
-	
-		<nav class="navbar navbar-default navbar-fixed-top">
-			<div class="contaianer-fluid">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mainNavBar">
+	</div>
+
+
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="contaianer-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mainNavBar">
 						<span class="sr-only"></span>
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-				</div>
-				<div class="collapse navbar-collapse" id="mainNavBar">
-					<ul class="nav nav-tab nav-justified">
-						<li><a href="index.php">Home</a></li>
-						<li><a href="gallery.php">Collections</a></li> 	
-						<li><a href="services.php">Services</a></li>						
-						<li class="active"><a href="contact.php">Contact</a></li>	
-					</ul>			
-				</div>
-			</div>		
-		</nav>
-		
-	<?php
+			</div>
+			<div class="collapse navbar-collapse" id="mainNavBar">
+				<ul class="nav nav-tab nav-justified">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="gallery.php">Collections</a></li>
+					<li><a href="services.php">Services</a></li>
+					<li class="active"><a href="contact.php">Contact</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+		<?php
+         	 require_once('recaptchalib.php');
+          $publickey = "6Ldsrw0UAAAAAJ76-56yX16JTjPk9NezNI_blD0a"; // you got this from the signup page
+          echo recaptcha_get_html($publickey);
+      			  ?>
+					<?php
 		
 		
 				if(isset($_POST['email'])){
@@ -64,13 +73,10 @@
 					
 					echo 'form sucessfully submitted';
 				}
-		
-		
-		
-		
+
 		
 		?>
-		
+
 		<div class="container">
 			<div class="row">
 				<div class="col-md-2"></div>
@@ -78,45 +84,46 @@
 					<div class="well">
 						<div class="well well-sm">
 							<div class="get">
-								<h3 class="text-centre">Contact us </h3> <hr>
-									<p class="text-justified">Feel free to contact us by calling 0871389294 or 
-									by filling in the enquiry form bellow.
-									<br>
-									.....
-									</p></div>
-						</div>	
+								<h3 class="text-centre">Contact us </h3>
+								<hr>
+								<p class="text-justified">Feel free to contact us by calling 0871389294 or by filling in the enquiry form bellow.
+									<br> .....
+								</p>
+							</div>
+						</div>
 						<div class="row">
 							<form name="myForm" method='post' action='contact.php' class="form-horizontal">
-								<div class="form-group">
-									<label for="inputName" class="control-label col-sm-2" > Name:</label>
+									<div class="form-group">
+										<label for="inputName" class="control-label col-sm-2"> Name:</label>
 										<div class="col-sm-10">
 											<input type="text" size="20" class="form-control" id="inputName" placeholder="Enter name">
 										</div>
-								</div>
-								<div class="form-group">
-									<label for="inputE-mail" class="control-label col-sm-2">E-mail:</label>
+									</div>
+									<div class="form-group">
+										<label for="inputE-mail" class="control-label col-sm-2">E-mail:</label>
 										<div class="col-sm-10">
 											<input type="email" name=email class="form-control" id="inputE-mail" placeholder="Enter e-mail">
 										</div>
-								</div>
-								<div class="form-group">
-									<label class="control-label col-sm-2" >Phone no:</label>
+									</div>
+									<div class="form-group">
+										<label class="control-label col-sm-2">Phone no:</label>
 										<div class="col-sm-10">
 											<input type="number" min="1" max="15" class="form-control" id="inputPhoneNumber" placeholder="Enter phone number">
 										</div>
-								</div>	
-								<div class="form-group">
-									<label for="inputMessage" class="control-label col-sm-2">Message:</label>
+									</div>
+									<div class="form-group">
+										<label for="inputMessage" class="control-label col-sm-2">Message:</label>
 										<div class="col-sm-10">
 											<textarea onfocus="thanks();" class="form-control" rows="7" id="inputMessage" placeholder="Enter message"></textarea>
 										</div>
+									</div>
+									<div class="form-group">
+										<div class="col-sm-offset-2 col-sm-10">
+											<button type="submit" name="fname" onsubmit="return validateForm()" class="btn btn-default">Submit</button>
+										</div>
+									</div>								
+									<div class="g-recaptcha" data-sitekey="6Ldsrw0UAAAAAJ76-56yX16JTjPk9NezNI_blD0a">
 								</div>
-								<div class="form-group">
-									<div class="col-sm-offset-2 col-sm-10">
-										 <button type="submit" name="fname" onsubmit="return validateForm()" class="btn btn-default">Submit</button>
-									</div>								 
-								</div>
-								<div class="g-recaptcha" data-sitekey="6Ldsrw0UAAAAAJ76-56yX16JTjPk9NezNI_blD0a"></div>
 							</form>
 						</div>
 					</div>
@@ -124,28 +131,26 @@
 				<div class="col-md-2"></div>
 			</div>
 		</div>
-		
+
 		<div class="alt2">
 			<footer class="container-fluid text-center">
 				<h2 class="text-center"><em>Music-<span>Zone</span></em></h2>
-					<p class="text-justified">
-											Military Road, Rathmines, Dublin<br>
-											Tel: 0871389294
-											<br>
+				<p class="text-justified">
+					Military Road, Rathmines, Dublin<br> Tel: 0871389294
+					<br>
 					<a href="mailto:cameliafarcas.cf@gmail.com"><span class="glyphicon glyphicon-envelope"></span></a>
-					</p>
+				</p>
 				<hr>
 				<p>&copy;2015 by Team J<br/></p>
 			</footer>
-		</div>	
-		
+		</div>
+
 
 		<script>
-			function thanks()
-			{
+			function thanks() {
 				alert("Thank you for visiting our site");
 			}
-			
+
 			function validateForm() {
 				var x = document.forms["myForm"]["fname"].value;
 				if (x == null || x == "") {
@@ -153,10 +158,10 @@
 					return false;
 				}
 			}
-			
-			
 		</script>
-	
-		<script src="js/bootstrap.min.js"> </script>
-	</body>
+
+		<script src="js/bootstrap.min.js">
+		</script>
+</body>
+
 </html>
