cb41a824a84ea279719999c0789cc47b4a255f5e
x15014657 <x15014657@student.ncirl.ie>
2016-12-02 17:20:08 -0500

    conor

---
+++ verify.php
@@ -0,0 +1,7 @@
+<?php
+         	 require_once('recaptchalib.php');
+          $publickey = "6Ldsrw0UAAAAAJ76-56yX16JTjPk9NezNI_blD0a"; // you got this from the signup page
+          echo recaptcha_get_html($publickey);
+      			  ?>
+					<?php
+		
