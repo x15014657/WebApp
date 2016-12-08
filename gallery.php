6ce44df474cbdeda234e63fbd79c843e1218b4cd
x15014657 <x15014657@student.ncirl.ie>
2016-12-08 15:08:30 -0500

    conor

--- gallery.php
+++ gallery.php
@@ -87,30 +87,35 @@
 	<div class="container">
 		<div class="gallery">
 			<div class="row padding">
-			
-				<?php
-
-$xml=simplexml_load_file("albums.xml");
-
-echo '<div>';
-foreach ($xml->cd as $cd)  
-{  
-   echo '<div>';
-   
-     
-   echo $cd['TITLE'], '  ';  
-   
-   echo $cd->ARTIST, '  ';  
-   echo $cd->COUNTRY, '<br />'; 
-   echo $cd->R_LABEL, '  ';  
-   echo $cd->PRICE, '<br />';
-   echo $cd->YEAR, '  ';
-   echo '<img src='.$CD->image.'/>';
-   echo '<scr='.$cd->SUBSCRIBE.'/>';
-   echo '</div>
-}  
-echo '</div>';
-?>
+				<div>
+					
+				</div>
+		<script>
+function loadXMLDoc() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      myFunction(this);
+    }
+  };
+  xmlhttp.open("GET", "album.xml", true);
+  xmlhttp.send();
+}
+function myFunction(xml) {
+  var i;
+  var xmlDoc = xml.responseXML;
+  var table="<tr><th>Artist</th><th>Title</th></tr>";
+  var x = xmlDoc.getElementsByTagName("CD");
+  for (i = 0; i <x.length; i++) { 
+    table += "<tr><td>" +
+    x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue +
+    "</td><td>" +
+    x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
+    "</td></tr>";
+  }
+  document.getElementById("demo").innerHTML = table;
+}
+				</script></div>
 			
 				
 			</div>
